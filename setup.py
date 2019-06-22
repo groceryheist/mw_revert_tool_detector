94004a6805174aa0c8664972dfa0590826d64b57
groceryheist <nathante@uw.edu>
2019-06-21 18:17:53 -0700

    update version

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 setup(name="mwcomments",
-      version="0.0.1",
+      version="0.0.2",
       description="Utilities for interpreting comments in edits to mediawiki wikis",
       license="GPL3",
       packages=['mwcomments'],
-      package_data={'mwcomments':['resources/wiki_patterns.json']},
+      package_data={'mwcomments':['resources/wiki_patterns.json',"resources/wikimedia_sites.json"]},
       author="Nathan TeBlunthuis",
       author_email="nathante@uw.edu",
       keywords=['mediawiki'],
       url=['https://github.com/groceryheist/python-mwcomments'],
-      install_requires=['mwapi','beautifulsoup4']
+      install_requires=['mwapi','beautifulsoup4','lxml']
 )
 

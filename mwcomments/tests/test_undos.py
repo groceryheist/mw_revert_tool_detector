0b11463c6f72c83f92f9d3f5efa48b9b843afab2
groceryheist <nathante@uw.edu>
2019-06-24 13:31:08 -0700

    update wiki patterns and tests

--- mwcomments/tests/test_undos.py
+++ mwcomments/tests/test_undos.py
@@ -11,7 +11,7 @@ class TestUndos(unittest.TestCase):
         self.assertSetEqual(set(match(message,'enwiki')),{"twinkle"})
 
     def test_undo(self):
-        message = "Undid revision 902317645 by 148.255.110.193 (talk) Unsourced addition and ethnicities do not go in the lead"
+        message = "Undid revision 902317645 by [[Special:Contributions/148.255.110.193|148.255.110.193]] ([[User talk:148.255.110.193|talk]]) Unsourced addition and ethnicities do not go in the lead"
         self.assertSetEqual(set(match(message,'enwiki')),{"undo"})
 
         message = 'Undid revision 862258093 by [[Special:Contributions/110.141.35.105|110.141.35.105]] ([[User talk:110.141.35.105|talk]])'
@@ -19,5 +19,9 @@ class TestUndos(unittest.TestCase):
         self.assertSetEqual(set(match(message,'enwiki')),{'undo'})
         
     def test_rollback(self):
-        message = "Reverted edits by Justthefacts98 (talk) to last revision by Someguy1221 (HG)"
+        message = "Reverted edits by [[Special:Contribs/Justthefacts98|Justthefacts98]] ([[User talk:Justthefacts98|talk]]) to last version by Someguy1221 (HG)"
         self.assertSetEqual(set(match(message,'enwiki')),{'huggle','rollback'})
+
+    def test_fr_undo(self):
+        message = "Annulation de la [[Special:Diff/$1|modification]] de [[Special:Contributions/$2|$2]] ([[User talk:$2|d]])"
+        self.assertSetEqual(set(match(message,'frwiki')),{'undo'})

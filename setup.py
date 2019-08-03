cc57258e71348fa885896da51e815b4db0c0751a
groceryheist <nathante@uw.edu>
2019-08-03 11:23:22 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,13 @@
 from setuptools import setup
 setup(name="mwcomments",
-      version="0.0.5",
+      version="0.1.1",
       description="Utilities for interpreting comments in edits to mediawiki wikis",
       license="GPL3",
       packages=['mwcomments'],
-      package_data={'mwcomments':['resources/wiki_patterns.json',"resources/wikimedia_sites.json"]},
+      package_data={'mwcomments':['resources/wiki_patterns.pickle',"resources/wikimedia_sites.pickle"]},
       author="Nathan TeBlunthuis",
       author_email="nathante@uw.edu",
       keywords=['mediawiki'],
-      url=['https://github.com/groceryheist/python-mwcomments'],
-      install_requires=['mwapi','beautifulsoup4','lxml','gitpython',"python-dateutil",'wikitextparser']
+      url='https://github.com/groceryheist/mw_revert_tool_detector',
+      install_requires=['mwapi','beautifulsoup4','lxml','gitpython',"python-dateutil",'wikitextparser','sortedcontainers', 'python-dateutil']
 )
-

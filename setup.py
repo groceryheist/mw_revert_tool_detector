26a7139913fc7f9a8d39c35e29be068492d6913e
groceryheist <nathante@uw.edu>
2019-08-08 16:32:12 -0700

    update setup.py with dependency versions and incerement mwcomments version

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 setup(name="mwcomments",
-      version="0.1.3",
+      version="0.1.4",
       description="Utilities for interpreting comments in edits to mediawiki wikis",
       license="GPL3",
       packages=['mwcomments'],
@@ -9,5 +9,5 @@ setup(name="mwcomments",
       author_email="nathante@uw.edu",
       keywords=['mediawiki'],
       url='https://github.com/groceryheist/mw_revert_tool_detector',
-      install_requires=['mwapi','lxml','gitpython',"python-dateutil>=2.7.3",'wikitextparser','sortedcontainers', 'python-dateutil']
+      install_requires=['mwapi>=0.5.1','lxml>=4.3.0','gitpython>=2.1.11',"python-dateutil>=2.7.3",'wikitextparser>=0.26.1','sortedcontainers>=2.1.0', 'python-dateutil>=2.7.3']
 )

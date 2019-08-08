d22e59862d87917ca3dbbc1a422a1d85a31978fc
groceryheist <nathante@uw.edu>
2019-08-07 17:45:15 -0700

    update .pickle with correct sources

--- tests/runtest.sh
+++ tests/runtest.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 nose2 tests
-#nose2 mwcomments.tests.test_wikitextToRegex.TestEnWiktionary
-#nose2 mwcomments.tests.test_wikitextToRegex.TestKK
-#nose2 mwcomments.tests.test_wikiToolMap.TestFromAllSources.test_kkwiki
-#nose2 mwcomments.tests.test_util  mwcomments.tests.test_wikitextToRegex
+#nose2 tests.test_wikitextToRegex.TestEnWiktionary
+#nose2 tests.test_wikitextToRegex.TestKK
+#nose2 tests.test_wikiToolMap.TestFromAllSources.test_kkwiki
+#nose2 tests.test_util  mwcomments.tests.test_wikitextToRegex

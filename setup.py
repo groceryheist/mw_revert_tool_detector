eb536ca69fc5a463060e5d0c11d640cc866dd6ce
groceryheist <nathante@uw.edu>
2019-08-13 17:57:11 +0000

    update module and dependencies so things work in spark

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name="mwcomments",
       author_email="nathante@uw.edu",
       keywords=['mediawiki'],
       url='https://github.com/groceryheist/mw_revert_tool_detector',
-      install_requires=['mwapi>=0.5.1','lxml','gitpython>=2.1.11',"python-dateutil>=2.7.3",'wikitextparser>=0.26.1','sortedcontainers>=2.1.0', 'python-dateutil>=2.7.3', 'nose2']
+      install_requires=['mwapi>=0.5.1','lxml','gitpython>=2.1.11',"python-dateutil>=2.7.3",'wikitextparser>=0.26.1','sortedcontainers>=2.1.0', 'python-dateutil>=2.7.3']
 )
